e317987a81b55d12089f0d2c74da72c9c27b8e8b
PavanKM07 <124868138+PavanKM07@users.noreply.github.com>
2023-02-10 11:44:38 +0530

    Add files via upload

---
+++ public/js/firebase.js
@@ -0,0 +1,20 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyDamFwaiMJ9e5XCZiSIB26vBmbuD-CguB4",
+
+    authDomain: "blog-482cc.firebaseapp.com",
+
+    projectId: "blog-482cc",
+
+    storageBucket: "blog-482cc.appspot.com",
+
+    messagingSenderId: "624040410219",
+
+    appId: "1:624040410219:web:e4c0f61931ed1dec57c52e",
+
+    measurementId: "G-Q20JH92Q49"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
